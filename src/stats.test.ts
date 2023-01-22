c1e0e481058ca45139a997bf64853082e1189ea0
Mario Pinto <mario@leanmind.es>
2023-01-22 16:59:31 +0000

    FIX: declaration of variables in order to encapsulate each test

--- src/stats.test.ts
+++ src/stats.test.ts
@@ -1,15 +1,14 @@
 import { average, sum } from './stats';
 import { expect, test } from './lib_tests';
 
-let result, expected;
 test('Sum should sum all numbers given', () => {
-	result = sum([1, 2, 3]);
-	expected = 6;
+	const result = sum([1, 2, 3]);
+	const expected = 6;
 	expect(expected).toBe(result);
 });
 
 test('Average should calculate average of all numbers given', () => {
-	result = average([1, 2, 3]);
-	expected = 2;
+	const result = average([1, 2, 3]);
+	const expected = 2;
 	expect(expected).toBe(result);
 });
