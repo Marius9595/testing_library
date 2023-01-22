1794c0d235f7cc92d9d0a1c79c14f98851349e84
Mario Pinto <mario@leanmind.es>
2023-01-22 21:57:12 +0000

    IMPROVEMENT: Added grouping of tests as Jest with describe function

--- src/lib_tests.ts
+++ src/lib_tests.ts
@@ -1,3 +1,10 @@
+/*
+
+TODO:
+ * Enseñar la línea en la que falla el test y donde donde se laza el error de código
+ * Permitir el soporte para asycn con la agrupación
+*/
+
 export function expect<T>(expected: T) {
 	return {
 		toBe(result: T) {
@@ -16,4 +23,11 @@ export async function test(description: string, callback: () => void) {
 		console.log(`FAILED: ${description}`);
 		console.log(error);
 	}
+}
+
+export const it = test;
+
+export function describe(description:string, callback:()=>void) {
+	console.log(description);
+	callback();
 }
--- src/stats.test.ts
+++ src/stats.test.ts
@@ -1,27 +1,31 @@
 import { average, sum } from './stats';
-import { expect, test } from './lib_tests';
+import { expect, test, describe, it } from './lib_tests';
 import * as stats_async from './stats_async';
 
-test('Sum should sum all numbers given', () => {
-	const result = sum([1, 2, 3]);
-	const expected = 6;
-	expect(expected).toBe(result);
+describe('tests sync', () => {
+	it('Sum should sum all numbers given', () => {
+		const result = sum([1, 2, 3]);
+		const expected = 6;
+		expect(expected).toBe(result);
+	});
+
+	it('Average should calculate average of all numbers given', () => {
+		const result = average([1, 2, 3]);
+		const expected = 2;
+		expect(expected).toBe(result);
+	});
 });
 
-test('Average should calculate average of all numbers given', () => {
-	const result = average([1, 2, 3]);
-	const expected = 2;
-	expect(expected).toBe(result);
-});
+describe('tests async', () => {
+	it('Sum should sum all numbers given', async () => {
+		const result = await stats_async.sum([1, 2, 3]);
+		const expected = 6;
+		expect(expected).toBe(result);
+	});
 
-test('Sum should sum all numbers given', async () => {
-	const result = await stats_async.sum([1, 2, 3]);
-	const expected = 6;
-	expect(expected).toBe(result);
-});
-
-test('Average should calculate average of all numbers given asynchronously', async () => {
-	const result = await stats_async.average([1, 2, 3]);
-	const expected = 2;
-	expect(expected).toBe(result);
+	it('Average should calculate average of all numbers given asynchronously', async () => {
+		const result = await stats_async.average([1, 2, 3]);
+		const expected = 2;
+		expect(expected).toBe(result);
+	});
 });
