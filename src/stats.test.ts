eb7545a9d6b816bff93d1ce395c719f5ee3f6eb9
Mario Pinto <mario@leanmind.es>
2023-01-22 16:32:27 +0000

    Abstraction expect to resolve duplicity (as jest library did)

--- src/stats.test.ts
+++ src/stats.test.ts
@@ -5,17 +5,23 @@ let result, expected, test_name;
 test_name = 'test sum';
 result = sum([1, 2, 3]);
 expected = 6;
-if (result == expected) {
-	console.log(`${test_name} : GOOD`);
-} else {
-	throw new Error(`${test_name} :  FAILED: ${result} is not equal to ${expected}`);
-}
+console.log(test_name);
+expect(expected).toBe(result);
 
 test_name = 'test average';
 result = average([1, 2, 3]);
 expected = 2;
-if (result == expected) {
-	console.log(`${test_name} : GOOD`);
-} else {
-	throw new Error(`${test_name} : FAILED: ${result} is not equal to ${expected}`);
+console.log(test_name);
+expect(expected).toBe(result);
+
+function expect(expected) {
+	return {
+		toBe(result) {
+			if (result == expected) {
+				console.log('GOOD');
+			} else {
+				throw new Error(`FAILED: ${result} is not equal to ${expected}`);
+			}
+		},
+	};
 }
