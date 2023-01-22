e8a15bee047860be625581931e6de3992895f168
Mario Pinto <mario@leanmind.es>
2023-01-22 16:57:12 +0000

    IMPROVEMENT: independent execution of test and more descriptive feedback

--- src/lib_tests.ts
+++ src/lib_tests.ts
@@ -1,11 +1,19 @@
 export function expect<T>(expected: T) {
 	return {
 		toBe(result: T) {
-			if (result == expected) {
-				console.log('GOOD');
-			} else {
+			if (result !== expected) {
 				throw new Error(`FAILED: ${result} is not equal to ${expected}`);
 			}
 		},
 	};
+}
+
+export function test(description: string, callback: () => void) {
+	try {
+		callback();
+		console.log(`GOOD: ${description}`);
+	} catch (error) {
+		console.log(`FAILED: ${description}`);
+		console.log(error);
+	}
 }
--- src/stats.test.ts
+++ src/stats.test.ts
@@ -1,16 +1,15 @@
 import { average, sum } from './stats';
-import { expect } from './lib_tests';
+import { expect, test } from './lib_tests';
 
-let result, expected, test_name;
+let result, expected;
+test('Sum should sum all numbers given', () => {
+	result = sum([1, 2, 3]);
+	expected = 6;
+	expect(expected).toBe(result);
+});
 
-test_name = 'test sum';
-result = sum([1, 2, 3]);
-expected = 6;
-console.log(test_name);
-expect(expected).toBe(result);
-
-test_name = 'test average';
-result = average([1, 2, 3]);
-expected = 2;
-console.log(test_name);
-expect(expected).toBe(result);
+test('Average should calculate average of all numbers given', () => {
+	result = average([1, 2, 3]);
+	expected = 2;
+	expect(expected).toBe(result);
+});
